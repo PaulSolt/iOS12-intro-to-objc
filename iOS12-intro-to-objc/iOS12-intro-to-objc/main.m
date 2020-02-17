57334a3db2a22450f9028fcee6cfe0dbea2ec667
Paul Solt <PaulSolt@gmail.com>
2020-02-17 12:12:50 -0500

    Added starter project

---
+++ iOS12-intro-to-objc/iOS12-intro-to-objc/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  iOS12-intro-to-objc
+//
+//  Created by Paul Solt on 2/17/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
